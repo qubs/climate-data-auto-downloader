5b028d01c58dd1e7172ba7d53378e739aaf39a50
David Lougheed <david.lougheed@gmail.com>
2016-08-15 15:44:19 -0400

    Add license, add rest of stations to configuration file and remove needless decimal specification on client, remove psycopg2 dependency, add some better logging and fetch messages for all stations at once.

--- autodownload.py
+++ autodownload.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os, sys, json, psycopg2, requests, datetime, time
+#   Copyright 2016 the Queen's University Biological Station
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import os, sys, json, requests, datetime, time, pprint
 from sudecode.sudecode import decode
 
 CONFIG_FILE_PATH = "./config.json"
+SEARCH_FILE_PATH = "./st.sc"
 MESSAGE_FILE_PATH = "./latest-messages.txt"
 
+GOES_DATA_CHANNEL = 19
 CENTURY_PREFIX = "20" # Hopefully this script won't be used in the 22nd century but just in case...
+
+pp = pprint.PrettyPrinter(indent=4, compact=True) # For nice log file output and debugging.
 
 def main():
     with open(CONFIG_FILE_PATH, "rU") as configFile:
         config = json.loads(configFile.read())
+
+        # TODO: Edit search terms configuration file for each station.
 
         os.system("getDcpMessages -u {} -P {} -h {} -p {} -f ./st.sc -x > {}".format(
             config["lrgsConnection"]["username"],
@@ -23,110 +43,125 @@ def main():
         ))
 
         with open(MESSAGE_FILE_PATH, "rU") as message_file:
-            cleaned_message = message_file.read().strip() # TODO: Remove newlines
+            # Remove any messages that are just spaces or blank lines.
+            messages_iterable = filter(None, map(str.strip, message_file.read().strip().split("\n")))
 
-            message_headers = cleaned_message[:37]
-            message_data = cleaned_message[37:]
+            for m in messages_iterable:
+                cleaned_message = m
 
-            goes_id = message_headers[:8]
+                message_headers = cleaned_message[:37]
+                message_data = cleaned_message[37:]
 
-            # Format: YYYYDDDHHMMSS. Convert it to ISO 8601 for the API.
-            arrival_time_text = message_headers[8:19]
-            arrival_time_object = datetime.datetime.strptime(CENTURY_PREFIX + arrival_time_text, "%Y%j%H%M%S")
+                goes_id = message_headers[:8]
+                goes_channel = int(message_headers[26:29])
 
-            message = {
-                "goes_id": goes_id,
-                "arrival_time": arrival_time_object.isoformat(),
-                "failure_code": message_headers[19:20],
-                "signal_strength": int(message_headers[20:22]),
-                "frequency_offset": message_headers[22:24],
-                "modulation_index": message_headers[24:25],
-                "data_quality": message_headers[25:26],
-                "goes_channel": int(message_headers[26:29]),
-                "spacecraft": message_headers[29:30],
-                "data_source": message_headers[30:32],
-                "recorded_message_length": int(message_headers[32:37]),
+                # Format: YYYYDDDHHMMSS. Convert it to ISO 8601 for the API.
+                arrival_time_text = message_headers[8:19]
+                arrival_time_object = datetime.datetime.strptime(CENTURY_PREFIX + arrival_time_text, "%Y%j%H%M%S")
 
-                "values": decode(message_data, 3,
-                    len(config["goesStations"][goes_id]["sensors"]),
-                    config["goesStations"][goes_id]["numReadings"]
-                ),
+                # We only want to process data from the data channel (GOES 19).
+                if goes_channel == GOES_DATA_CHANNEL:
+                    message = {
+                        "goes_id": goes_id,
+                        "arrival_time": arrival_time_object.isoformat(),
+                        "failure_code": message_headers[19:20],
+                        "signal_strength": int(message_headers[20:22]),
+                        "frequency_offset": message_headers[22:24],
+                        "modulation_index": message_headers[24:25],
+                        "data_quality": message_headers[25:26],
+                        "goes_channel": goes_channel,
+                        "spacecraft": message_headers[29:30],
+                        "data_source": message_headers[30:32],
+                        "recorded_message_length": int(message_headers[32:37]),
 
-                "message_text": cleaned_message
-            }
+                        "values": decode(message_data, 3,
+                            len(config["goesStations"][goes_id]["sensors"]),
+                            config["goesStations"][goes_id]["numReadings"]
+                        ),
 
-            print(message)
-
-            # TODO: Process more (into readings, sensors, etc.)
-            # TODO: Check for duplicate messages
-
-            # Check for repeat messages.
-
-            duplicate_exists = False
-
-            try:
-                r = requests.get(
-                    "{}/messages/".format(config["apiConnection"]["url"]),
-                    params={
-                        "goes_id": message["goes_id"],
-                        "start": arrival_time_object - datetime.timedelta(seconds=1),
-                        "end": arrival_time_object + datetime.timedelta(seconds=1)
+                        "message_text": cleaned_message
                     }
-                )
 
-                result = r.json()
+                    pp.pprint(message)
 
-                if len(result) > 0:
-                    print("{}: The message already has been saved.".format(datetime.datetime.now().isoformat()))
-                    duplicate_exists = True
+                    # TODO: Process more (into readings, sensors, etc.)
+                    # TODO: Check for duplicate messages
 
-            except requests.exceptions.Timeout:
-                print("{}: Time-out while checking for duplicate messages.".format(datetime.datetime.now().isoformat()))
+                    # Check for repeat messages.
 
-            except ValueError:
-                print("{}: An invalid JSON response was recieved.".format(datetime.datetime.now().isoformat()))
-
-            except requests.exceptions.RequestException as e:
-                # Catch any generic request errors.
-
-                print("{}: Error while checking for duplicate messages.".format(datetime.datetime.now().isoformat()))
-                print(e)
-
-            # If a duplicate doesn't exist, post the message to the API.
-
-            if not duplicate_exists:
-                times_to_repeat = 1
-                cancel_next_repeat = False
-
-                while times_to_repeat > 0:
-                    times_to_repeat -= 1
+                    duplicate_exists = False
 
                     try:
-                        # Add message to database.
-
-                        r = requests.post(
+                        r = requests.get(
                             "{}/messages/".format(config["apiConnection"]["url"]),
-                            data=message,
-                            auth=(config["apiConnection"]["username"], config["apiConnection"]["password"])
+                            params={
+                                "goes_id": message["goes_id"],
+                                "start": arrival_time_object - datetime.timedelta(seconds=1),
+                                "end": arrival_time_object + datetime.timedelta(seconds=1)
+                            }
                         )
 
+                        result = r.json()
+
+                        if len(result) > 0:
+                            print("{}: The following message already has been saved: {}.".format(
+                                datetime.datetime.now().isoformat(),
+                                cleaned_message
+                            ))
+                            duplicate_exists = True
+
                     except requests.exceptions.Timeout:
-                        # Try a single retry.
+                        print("{}: Time-out while checking for duplicate messages.".format(datetime.datetime.now().isoformat()))
 
-                        if cancelNextRepeat:
-                            times_to_repeat = 1
-                            cancel_next_repeat = True
-
-                            # Delay retry for 30 seconds to give the server some time to figure stuff out.
-                            time.sleep(30)
+                    except ValueError:
+                        print("{}: An invalid JSON response was recieved.".format(datetime.datetime.now().isoformat()))
 
                     except requests.exceptions.RequestException as e:
                         # Catch any generic request errors.
 
-                        print("{}: Automatic download of data failed.".format(datetime.datetime.now().isoformat()))
+                        print("{}: Error while checking for duplicate messages.".format(datetime.datetime.now().isoformat()))
                         print(e)
 
-                        sys.exit(1)
+                    # If a duplicate doesn't exist, post the message to the API.
+
+                    if not duplicate_exists:
+                        times_to_repeat = 1
+                        cancel_next_repeat = False
+
+                        while times_to_repeat > 0:
+                            times_to_repeat -= 1
+
+                            try:
+                                # Add message to database.
+
+                                r = requests.post(
+                                    "{}/messages/".format(config["apiConnection"]["url"]),
+                                    data=message,
+                                    auth=(config["apiConnection"]["username"], config["apiConnection"]["password"])
+                                )
+
+                            except requests.exceptions.Timeout:
+                                # Try a single retry.
+
+                                if cancelNextRepeat:
+                                    times_to_repeat = 1
+                                    cancel_next_repeat = True
+
+                                    # Delay retry for 30 seconds to give the server some time to figure stuff out.
+                                    time.sleep(30)
+
+                            except requests.exceptions.RequestException as e:
+                                # Catch any generic request errors.
+
+                                print("{}: Automatic download of data failed.".format(datetime.datetime.now().isoformat()))
+                                print(e)
+
+                                sys.exit(1)
+                else:
+                    print("{}: Downloaded the following non-data message: {}".format(
+                        datetime.datetime.now().isoformat(),
+                        cleaned_message
+                    ))
 
 if __name__ == "__main__":
     main()
