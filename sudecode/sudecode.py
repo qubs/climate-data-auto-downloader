9295ef25ba414ea75132161b3ff9183b1979d662
David Lougheed <david.lougheed@gmail.com>
2017-07-04 18:49:15 -0400

    Instead of padding an arbitrary amount of Nones for a content-less message, don't return any values from the decoder.

--- sudecode/sudecode.py
+++ sudecode/sudecode.py
@@ -12,11 +12,11 @@ def decode(encoded_message, bytes_per_value, num_sensors, num_readings):
     while encoded_message[message_start] != "B":
         message_start += 1
 
-    invalidate_everything = False
+    no_content = False
 
     # Check for an empty data buffer, which may get converted into "made-up" values.
     if SATLINK_DATA_BUFFER_EMPTY in encoded_message:
-        invalidate_everything = True
+        no_content = True
 
     # group_id = encoded_message[message_start + 1]
     # time_offset = ord(encoded_message[message_start + 2]) - 64
@@ -27,49 +27,50 @@ def decode(encoded_message, bytes_per_value, num_sensors, num_readings):
 
     # TODO: Handle negative numbers.
 
-    while data_offset < initial_data_offset + num_sensors * bytes_per_value * num_readings:
-        if data_offset > len(encoded_message) - 1:  # Extra data on end, TODO: Extra data variable
-            # Terminate early, this is an incomplete transmission.
-            break
-
-        data_bytes = encoded_message[data_offset:data_offset + bytes_per_value]
-
-        # TODO: Allow for variable bytes per value
-
-        no_value = False
-        byte_offsets = [ord(x) - 64 for x in data_bytes]
-
-        for i in range(len(data_bytes) - 1, -1, -1):
-            if data_bytes[i] != "/":
+    if not no_content:
+        while data_offset < initial_data_offset + num_sensors * bytes_per_value * num_readings:
+            if data_offset > len(encoded_message) - 1:  # Extra data on end, TODO: Extra data variable
+                # Terminate early, this is an incomplete transmission.
                 break
-            elif data_bytes[i] == "/" and i == 0:
-                no_value = True
 
-        for i, byte in enumerate(byte_offsets):
-            if byte < -1 or byte > 63:
-                no_value = True
+            data_bytes = encoded_message[data_offset:data_offset + bytes_per_value]
 
-        if not (no_value or invalidate_everything):
-            # The value can be considered 'valid' for the purposes of the decoder.
+            # TODO: Allow for variable bytes per value
 
-            # The pseudobinary format sends the value 127 (ASCII delete) as a question mark (63) instead.
-            # We move them back up to their correct numerical value.
-            for i, byte in enumerate(byte_offsets):
-                if byte == -1:
-                    byte_offsets[i] = 63
+            no_value = False
+            byte_offsets = [ord(x) - 64 for x in data_bytes]
 
-            numerical_value = 0
             for i in range(len(data_bytes) - 1, -1, -1):
-                numerical_value += byte_offsets[i] * 2 ** ((len(data_bytes) - i - 1) * 6)
+                if data_bytes[i] != "/":
+                    break
+                elif data_bytes[i] == "/" and i == 0:
+                    no_value = True
 
-            if numerical_value > (2 ** (bytes_per_value * 6) // 2) - 1:  # i.e. is a negative number in two's complement
-                numerical_value = -1 * (2 ** (bytes_per_value * 6) - numerical_value)
+            for i, byte in enumerate(byte_offsets):
+                if byte < -1 or byte > 63:
+                    no_value = True
 
-            decoded_data.append(numerical_value)
-        else:
-            decoded_data.append(None)  # None represents a blank field here
+            if not no_value:
+                # The value can be considered 'valid' for the purposes of the decoder.
 
-        data_offset += bytes_per_value
+                # The pseudobinary format sends the value 127 (ASCII delete) as a question mark (63) instead.
+                # We move them back up to their correct numerical value.
+                for i, byte in enumerate(byte_offsets):
+                    if byte == -1:
+                        byte_offsets[i] = 63
+
+                numerical_value = 0
+                for i in range(len(data_bytes) - 1, -1, -1):
+                    numerical_value += byte_offsets[i] * 2 ** ((len(data_bytes) - i - 1) * 6)
+
+                if numerical_value > (2 ** (bytes_per_value * 6) // 2) - 1:  # i.e. is a negative number in two's complement
+                    numerical_value = -1 * (2 ** (bytes_per_value * 6) - numerical_value)
+
+                decoded_data.append(numerical_value)
+            else:
+                decoded_data.append(None)  # None represents a blank field here
+
+            data_offset += bytes_per_value
 
     return decoded_data
 
